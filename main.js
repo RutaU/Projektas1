cab4137c756e81c8b9fc087e721fc2e9b04e0261
Ruta <ruta.urkiene@gmail.com>
2015-08-20 19:42:00 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	README.md
    
    changes
    
    web application

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-
-var button = document.getElementById("interactive-button");
-button.addEventListener("click", function(){ alert("Wow") } , false);
+$(document).ready(function(){
+$("#interactive-button").on("click", function(){
+alert("Wow");
+});
+});
