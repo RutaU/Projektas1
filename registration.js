c410c053c8b72b391d287ad440e24df73f9eaedb
Ruta <ruta.urkiene@gmail.com>
2015-08-28 20:29:51 +0300

    Login form
    Login form

---
+++ registration.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".button").on("click", function(){
+		var username = $("#username").val();
+		var password = $("#psw").val();
+		var email = $("#email").val();
+		
+				$(".success").show();
+	}) 
+});
