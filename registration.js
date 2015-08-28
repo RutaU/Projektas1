ea4747143374027d80798b047508513ad0826296
Ruta <ruta.urkiene@gmail.com>
2015-08-28 23:29:36 +0300

    Changes in responsive design
    Changes in responsive design

--- registration.js
+++ registration.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 	$(".button").on("click", function(){
-		var username = $("#username").val();
+	var username = $("#username").val();
 		var password = $("#psw").val();
 		var email = $("#email").val();
-		
+			if (username.length == 0 || 
+				password.length == 0 || 
+				email.length == 0) {
+				alert ("Something missing here...");
+			}	else {
 				$(".success").show();
-	}) 
+					} 
+});
 });
